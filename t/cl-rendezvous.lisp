93ab727f0dafa31cca4b859bb922ffa1471c7b7f
Kazuo Koga <kogakazuo@gmail.com>
2011-09-23 17:17:03 +0900

    テスト追加

--- t/cl-rendezvous.lisp
+++ t/cl-rendezvous.lisp
@@ -12,6 +12,13 @@
 
 (plan nil)
 
-;; blah blah blah.
+(diag "Instance creation.")
+(ok (make-rendezvous))
+(ok (make-rendezvous "object name"))
+(ok (make-rendezvous 'object-name))
+(is (rendezvous-name (make-rendezvous "name")) "name")
+(is (rendezvous-name (make-rendezvous (string 'object-name))) "OBJECT-NAME")
+(ok (loop repeat 10000
+          collect (make-rendezvous)))
 
 (finalize)
